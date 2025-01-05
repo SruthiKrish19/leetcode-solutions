eb41f99fd18e90f914ea846a2338df7c9d4589c5
Sruthi Krishnakumar <76727733+SruthiKrish19@users.noreply.github.com>
2025-01-05 12:53:37 +0530

    584_Find_Customer_Referee

---
+++ sql50/584_Find_Customer_Referee.sql
@@ -0,0 +1,17 @@
+-- Write your PostgreSQL query statement below
+
+-- Schema
+Create table If Not Exists Customer (id int, name varchar(25), referee_id int)
+Truncate table Customer
+insert into Customer (id, name, referee_id) values ('1', 'Will', NULL)
+insert into Customer (id, name, referee_id) values ('2', 'Jane', NULL)
+insert into Customer (id, name, referee_id) values ('3', 'Alex', '2')
+insert into Customer (id, name, referee_id) values ('4', 'Bill', NULL)
+insert into Customer (id, name, referee_id) values ('5', 'Zack', '1')
+insert into Customer (id, name, referee_id) values ('6', 'Mark', '2')
+
+-- Solution 1
+select name from Customer c where c.referee_id != 2 OR c.referee_id is null
+
+-- Solution 2
+select distinct(name) from Customer c where coalesce(c.referee_id, 0) != 2 
