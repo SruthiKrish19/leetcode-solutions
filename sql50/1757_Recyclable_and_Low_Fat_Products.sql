c1689a6809d4022ac5505b16fbe68baee187f9c8
Sruthi Krishnakumar <76727733+SruthiKrish19@users.noreply.github.com>
2025-01-05 11:18:59 +0530

    1757_Recyclable_and_Low_Fat_Products

---
+++ sql50/1757_Recyclable_and_Low_Fat_Products.sql
@@ -0,0 +1,11 @@
+-- Table Schema
+Create table If Not Exists Products (product_id int, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'))
+Truncate table Products
+insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N')
+insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N')
+
+-- Solution Query
+select product_id from Products p where p.low_fats = 'Y' and p.recyclable = 'Y'
